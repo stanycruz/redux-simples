5e19db5a7726a7eb9a481f1cf8f55dc92e67b6e1
Stanislaw Cruz <stanycruz@outlook.com>
2021-12-09 12:11:54 -0300

    Iniciando a App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Card from './components/Card'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Exercício React-Redux (Simples)</h1>
+            <div className='linha'>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <div className='linha'>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
 }
 
 export default App;
