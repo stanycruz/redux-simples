10325821018e766b8a410061cd702ea41bdc0583
Stanislaw Cruz <stanycruz@outlook.com>
2021-12-12 16:10:11 -0300

    Outros Componentes

---
+++ src/components/Soma.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Card from './Card'
+
+export default props => {
+    return (
+        <Card title="Soma dos números" blue>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>10</strong>
+
+                </span>
+            </div>
+        </Card>
+    )
+}
